7cdf72dc998067bef56fc1cf796692d9190900a5
se_ga <jopa123JOPA>
2018-12-15 21:51:07 +0400

    set new DB

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,4 +1,5 @@
-INSERT INTO players VALUES (1, 'se_ga');
-INSERT INTO players VALUES (2, 'egor');
-INSERT INTO players VALUES (3, 'enjua');
-INSERT INTO players VALUES (4, 'shorin');
+INSERT INTO "public".players VALUES (1, 'se_ga');
+INSERT INTO "public".players VALUES (2, 'egor');
+INSERT INTO "public".players VALUES (3, 'enjua');
+INSERT INTO "public".players VALUES (4, 'shorin');
+INSERT INTO "public".sessions VALUES (1, 'test');
